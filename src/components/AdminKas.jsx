b11c449b09cafa611d0fb2351eba904210811d22
TwentyCloff <twencloff@gmail.com>
2025-07-24 18:13:52 +0700

    Update AdminKas.jsx

--- src/components/AdminKas.jsx
+++ src/components/AdminKas.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { db } from './firebase';
+import { db } from '../config/firebaseConfig';
 import { collection, query, onSnapshot, orderBy } from 'firebase/firestore';
 
 const Manage = () => {
