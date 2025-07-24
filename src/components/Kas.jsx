8a669fec911c2a1618fac83e0a76065fbf969b36
TwentyCloff <twencloff@gmail.com>
2025-07-24 18:14:25 +0700

    Update Kas.jsx

--- src/components/Kas.jsx
+++ src/components/Kas.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { db } from './firebase';
+import { db } from '../config/firebaseConfig';
 import { doc, setDoc, serverTimestamp } from 'firebase/firestore';
 
 const names = [
